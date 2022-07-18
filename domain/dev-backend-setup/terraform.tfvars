25507afb48de2949740667eb25a3f0bcc4e40e6e
sanjay <sanjayrohilla@gmail.com>
2022-07-18 12:53:25 +1000

    AWS Account No remove and S3 bucket name change

--- domain/dev-backend-setup/terraform.tfvars
+++ domain/dev-backend-setup/terraform.tfvars
@@ -1,3 +1,3 @@
 env             = "dev"               
 app_name        = "BelongCodeTest"
-aws_acc_no      = "059114722599"      #Please update the AWS Account for deploying the application 
+aws_acc_no      = "xxxxxxxxxxxx"      #Please update the AWS Account for deploying the application 
--- modules/ec2/user-script/user-data.sh
+++ modules/ec2/user-script/user-data.sh
@@ -4,5 +4,4 @@ yum install httpd -y
 systemctl start httpd
 systemctl enable httpd
 timedatectl set-timezone ${timezone}
-#aws s3 cp s3://belong-coding-challenge/belong-test.html /var/www/html/index.html
-aws s3 cp s3://sanjay-rohilla/belong-test.html /var/www/html/index.html
+aws s3 cp s3://belong-coding-challenge/belong-test.html /var/www/html/index.html
